a87d95bad33c68b9c51d21c5c3374724a560c9af
Monicacholico <monicachol@hotmail.com>
2022-07-20 15:09:58 -0700

    initial commit

---
+++ PM/pm/js/counter.js
@@ -0,0 +1,78 @@
+export function setupCounter(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(++counter))
+  setCounter(0)
+}
+
+
+const articleTemplate = document.getElementById('article');
+const articleDisplayer = document.getElementById('articles');
+// const API_KEY = 7d3296e1e55d4d57a6c3c7c7e814f2fd;
+const executeApiEn = () => {
+  const url = `https://newsapi.org/v2/everything?q=gender&searchIn=title&apiKey=7d3296e1e55d4d57a6c3c7c7e814f2fd`;
+  const options = {
+    method: "GET",
+    header: {
+      accept: "application/json"
+    }
+  }
+  return fetch(url, options).then(
+    response => {
+      return response.json();
+    }
+  ).then(data => {
+    console.log(data);
+    const allArticles = data.articles;
+    const eachArticle = allArticles.map( article => {
+      const articleEl = document.importNode(articleTemplate.content, true);
+      console.log(articleEl)
+      articleEl.querySelector('h2').textContent = article.title;
+      articleEl.querySelector('p').textContent = article.content;
+      articleEl.querySelector('small').textContent = article.author;
+      articleDisplayer.append(articleEl);
+    })
+    // console.log(eachArticle);
+
+  })
+
+} 
+
+executeApiEn();
+
+const articleTemplateEs = document.getElementById('articleES');
+const articleDisplayerEs = document.getElementById('articlesEs');
+// const API_KEY = 7d3296e1e55d4d57a6c3c7c7e814f2fd;
+const executeApiEs = () => {
+  const url = `https://newsapi.org/v2/everything?q=genero&searchIn=title&language=es&apiKey=7d3296e1e55d4d57a6c3c7c7e814f2fd`;
+  const options = {
+    method: "GET",
+    header: {
+      accept: "application/json"
+    }
+  }
+  return fetch(url, options).then(
+    response => {
+      return response.json();
+    }
+  ).then(data => {
+    console.log(data);
+    const allArticlesEs = data.articles;
+    const eachArticleEs = allArticlesEs.map( articleEs => {
+      const articleElEs = document.importNode(articleTemplateEs.content, true);
+      console.log(articleElEs)
+      articleElEs.querySelector('h2').textContent = articleEs.title;
+      articleElEs.querySelector('p').textContent = articleEs.content;
+      articleElEs.querySelector('small').textContent = articleEs.author;
+      articleDisplayerEs.append(articleElEs);
+    })
+    // console.log(eachArticle);
+
+  })
+
+} 
+
+executeApiEs();
